25b12c3e64c192d49ee0b9daff0d9601e57cc626
Lokman DOĞAN <developer@lokmandogan.com>
2023-10-18 21:58:03 +0300

    Proje oluşturma ve ilk ders.

---
+++ with-Murat-Yucedag/Ders1/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Ders1
+//
+//  Created by Lokman DOĞAN on 18.10.2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
---
+++ with-Murat-Yucedag/with-Murat-Yucedag/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  with-Murat-Yucedag
+//
+//  Created by Lokman DOĞAN on 18.10.2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
