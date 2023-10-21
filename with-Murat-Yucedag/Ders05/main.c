160f1c9054895380f82c11555f615094ff0ef66c
Lokman DOĞAN <developer@lokmandogan.com>
2023-10-21 21:21:10 +0300

    Ders adlarını düzenleme.

---
+++ with-Murat-Yucedag/Ders05/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Ders5
+//
+//  Created by Lokman DOĞAN on 21.10.2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
