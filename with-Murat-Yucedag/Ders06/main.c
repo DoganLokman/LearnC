41b2d36c64e79786381d9820739c63d9c76e4d83
Lokman DOĞAN <developer@lokmandogan.com>
2023-10-22 21:27:51 +0300

    6. ders eklendi.

---
+++ with-Murat-Yucedag/Ders06/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Ders06
+//
+//  Created by Lokman DOĞAN on 22.10.2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
