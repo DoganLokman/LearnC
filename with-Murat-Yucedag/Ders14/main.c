9e8bc6ff25da54613e691cfb24e5396d69cdde64
Lokman DOĞAN <developer@lokmandogan.com>
2023-10-28 23:11:52 +0300

    14. ders eklendi....

--- with-Murat-Yucedag/Ders14/main.c
+++ with-Murat-Yucedag/Ders14/main.c
@@ -7,8 +7,62 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main() { // Buradaki int main() parantezinin içinin boş olma durumu kullanımdan kaldırıldı, ancak bu değerlerin anlatıldığı derse kadar biz boş kullanacağız.
+    // while DÖNGÜSÜ
+    /*
+     while (şart) {
+     
+     }
+     şeklinde tanımlanır.
+    */
+    /*
+    int i;
+    for (i = 1; i <= 5; i++) {
+        printf("Merhaba Lokman!\n");
+    }
+    */
+    /*
+    int i;
+    i=1;
+    while (i <= 5) {
+        printf("Merhaba Lokman!\n");
+        i++;
+    }
+    */
+    /*
+    // 1 ile 10 arasındaki sayıları while ile listeleyelim.
+    
+    int i = 1;
+    while (i <= 10) {
+        printf("%d\n", i);
+        i++;
+    }
+    */
+    /*
+    // 1 ile 20 arasındaki çift sayıları 14 hariç listeleyelim.
+    
+    int i = 1;
+    
+    while (i < 20) {
+        if (i % 2 == 0 && i != 14) {
+            printf("%d\n", i);
+        }
+        i++;
+    }
+    */
+    
+    // Girilen sayının faktöriyelini while ile hesaplatalım.
+    
+    int sayi, faktoriyel = 1;
+    
+    printf("Lütfen faktöriyelinin hesaplanmasını istediğiniz sayıyı giriniz: ");
+    scanf("%d", &sayi);
+    
+    while (sayi > 1) {
+        faktoriyel = faktoriyel * sayi;
+        sayi--;
+    }
+    printf("%d\n", faktoriyel);
+    
     return 0;
 }
