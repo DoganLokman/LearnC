7b5570fbc9222e32063f02453a101373fc351def
Lokman DOĞAN <developer@lokmandogan.com>
2023-10-30 00:27:24 +0300

    18. ders eklendi.

--- with-Murat-Yucedag/Ders18/main.c
+++ with-Murat-Yucedag/Ders18/main.c
@@ -7,8 +7,31 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main() { // Buradaki int main() parantezinin içinin boş olma durumu kullanımdan kaldırıldı, ancak bu değerlerin anlatıldığı derse kadar biz boş kullanacağız.
+    
+    /*
+    int sayilar[] = {3, 13, 7, 75, 58, 44};
+    
+    //printf("%d\n", sayilar[2]);
+    
+    int i;
+    
+    for (i = 0; i < 6; i++) {
+        printf("%d\n", sayilar[i]);
+    }
+    */
+    
+    int sayilar[] = {10, 15, 20, 35};
+    
+    int toplam = 0;
+    int i;
+    
+    for (i = 0; i < 4; i++) {
+        toplam = toplam + sayilar[i];
+    }
+    
+    printf("Toplam: %d\n", toplam);
+
+
     return 0;
 }
